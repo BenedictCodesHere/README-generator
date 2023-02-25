abc75c17a8e21c32fa71f8c235aa7de9211cb239
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-02-25 22:37:01 +0000

    created a separate module generateLicenseBadge.js to clean the code within index.js

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # README-generator
 An app designed using Node.js which enables a user to create a README for their project using their CLI or terminal.
+
+![Walkthrough](./walkthrough-video.mp4)
+
+## Installation
+This application runs using Node.js. Please make sure you have:
+1. Installed Node's LTS - latest stable version.
+2. Navigate to and open your project folder.
+3. Run npm init in your project folder to create your package.json.
+4. Run npm install inquirer (a dependency necessary for the program to run). 
+5. You will need the index.js and the utils folder downloaded from this repo, as this contains the application's code.
+6. Make sure that you have a .gitignore file in your project folder, and make sure it has "node_modules" added to the .gitignore.
+
+## Usage
+1. Make sure you have opened your project folder in either your command line or integrated terminal.
+2. Type "node index.js"
+3. Answer the questions that come up in your command line, to fill out your README.
+4. Once you have completed the questions, you should successfully have generated a README for your project.
+
+## Credits
+N/A
+
+## License
+Please refer to the license in the repo (MIT License).
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const generateLicenseBadge = require("./utils/generateLicenseBadge");
 
 
 // array of questions for user
@@ -63,58 +64,13 @@ function writeToFile(fileName, data) {
 };
 
 // function to create the license badge
-function licenseBadge(licenseName){
-  switch(licenseName){
-    case 'MIT':
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-      break;
-    case 'Apache':
-      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-      break;
-    case 'GNU':
-      return ``;
-      break;
-    case 'BSD 2-Clause':
-      return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
-      break;
-      case 'BSD 3-Clause':
-      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
-      break;
-      case 'Boost':
-      return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
-      break;
-      case 'Creative Commons Zero':
-      return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
-      break;
-      case 'Eclipse':
-      return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
-      break;
-      case 'GNU Affero':
-      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
-      break;
-      case 'GNU General':
-      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-      break;
-      case 'GNU Lesser':
-      return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
-      break;
-      case 'Mozilla Public':
-      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
-      break;
-      case 'The Unlicense':
-      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
-      break;
-      case 'none':
-      return ``;
-      break;
-  }
-}
+
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions)
     .then((response) => {
-      response.licenseBadge = licenseBadge(response.license);
+      response.licenseBadge = generateLicenseBadge(response.license);
       let text = generateMarkdown(response);
       writeToFile('README.md', text)
     }
